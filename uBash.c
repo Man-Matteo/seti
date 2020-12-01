90efca35b3c308ffe960acde8ede65c442462cc1
MatteMannai <matteo.mannai@outlook.it>
2020-12-01 17:54:23 +0100

    Update uBash.c

--- uBash.c
+++ uBash.c
@@ -1,56 +1,95 @@
-/* funzioni da implementare
-cd
-> / <
-grep
-|
-cat
-ls / ls -l
-
-cosa ci serve:
-funzione che legge da input e divide i comandi
-
-*/
-
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#define __USE_BSD
+#include <limits.h>
 
-const int PATH_MAX = 4096; //lib limits.h
-
-void mypwd() {
-   char cwd[PATH_MAX];
-   getcwd(cwd, sizeof(cwd));
-   printf(" %s $ ", cwd);
+void mypwd(char* cwd) {
+    getcwd(cwd, PATH_MAX);
 }
+
+void mycd(char* cwd, char* argument) {
+
+    if (strcmp(argument, "..") == 0) {
+        printf("Errore .. non disponibile\n");
+        _exit;
+    }
+
+    strcat(cwd,"/");
+    strcat(cwd, argument);
+    cwd[strcspn(cwd, "\n")] = 0;
+
+}
+
+
+int mytokenizer(char* a, char* s, char** array) {
+
+    char *token;
+    token = strtok(a, s);
+
+    int index = 0;
+    while( token != NULL ) {
+        array[index] = token;
+        index++;
+        token = strtok(NULL, s);
+    }
+
+    return index;
+}
+
 
 int main() {
 
-for (;;) {
 
-   //cartella corrente
-   mypwd();
+    char cwd[PATH_MAX];
+    mypwd(cwd);
 
-   //lettura stdin
-   char *a = NULL;
-   size_t alen = 0;
-   ssize_t alineSize = 0;
-   alineSize = getline(&a, &alen, stdin);
+    for (;;) {
 
-   //divisione dei token in array[]
-   const char s[2] = "|";
-   char *token;
-   char *array[alineSize];
+        //cartella corrente
+        printf("%s $ ", cwd);
 
-   token = strtok(a, s);
+        //lettura stdin
+        char *a = NULL;
+        size_t alen = 0;
+        ssize_t alineSize = 0;
+        alineSize = getline(&a, &alen, stdin);
 
-   int index = 0;
-   while( token != NULL ) {
-       printf( " %s\n", token );
-       array[index++] = token;
-       token = strtok(NULL, s);
+        //divisione dei token per spazi
+        char* blank[alineSize];
+        int index = mytokenizer(a, " ", blank);
+
+
+        if (strcmp(blank[0], "cd") == 0) {
+
+            if (index > 2) {
+                printf("Errore troppi argomenti per cd\n");
+                continue;
+            }
+           mycd(cwd, blank[1]);
+         }
+
+       }
    }
 
-  for (int i = 0; i < index; i++) printf("%s\n", array[i]);
 
-}
-}
+        /*for (scorro ogni carattere) {
+            if (trovo "$")
+              chiamo funzione $ e sostituisco comando
+            if (trovo "<" || trovo ">" )
+              reindirizzo input/output
+            if (trovo "|")
+              STDOUT comando 1 = STDIN comando 2
+        }
+
+          .....
+
+          for (ogni "parola token") {
+            if (comando 1)
+              chiamo funzione 1
+            if (comando 2)
+              chiamo funzione 2
+
+          .....
+          }
+        */
